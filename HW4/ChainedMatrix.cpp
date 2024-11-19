279a11e872f6e7ee30d431d8a7e36e98a486baf5
nakge <kogwanho3@gmail.com>
2024-11-19 14:20:48 +0900

    2.0

--- HW4/ChainedMatrix.cpp
+++ HW4/ChainedMatrix.cpp
@@ -8,18 +8,18 @@
 
 using namespace std;
 
-int matrixChainOrder(const vector<int>& dimensions, vector<vector<int>>& C) {
-    int n = dimensions.size() - 1;
-    for (int i = 1; i <= n; ++i) {
+int matrixChainOrder(vector<int> dimensions, vector<vector<int>>& C) {
+    int n = dimensions.size();
+    for (int i = 1; i < n; i++) {
         C[i][i] = 0;
     }
 
-    for (int L = 1; L < n; ++L) {
-        for (int i = 1; i <= n - L; ++i) {
+    for (int L = 1; L < n-1; L++) {
+        for (int i = 1; i < n - L; i++) {
             int j = i + L;
             C[i][j] = INT_MAX;
 
-            for (int k = i; k < j; ++k) {
+            for (int k = i; k < j; k++) {
                 int temp = C[i][k] + C[k + 1][j] + dimensions[i - 1] * dimensions[k] * dimensions[j];
                 if (temp < C[i][j]) {
                     C[i][j] = temp;
@@ -27,20 +27,20 @@ int matrixChainOrder(const vector<int>& dimensions, vector<vector<int>>& C) {
             }
         }
     }
-    return C[1][n];
+    return C[1][n-1];
 }
 
 void printMatrix(const vector<vector<int>>& C) {
-    int n = C.size() - 1;
+    int n = C.size();
     cout << "C\t";
-    for (int j = 1; j <= n; ++j) {
+    for (int j = 1; j < n; ++j) {
         cout << j << "\t";
     }
     cout << endl;
 
-    for (int i = 1; i <= n; ++i) {
+    for (int i = 1; i < n; ++i) {
         cout << i << "\t";
-        for (int j = 1; j <= n; ++j) {
+        for (int j = 1; j < n; ++j) {
             if (i > j) {
                 cout << "\t";
             } else if (C[i][j] == INT_MAX) {
@@ -53,10 +53,10 @@ void printMatrix(const vector<vector<int>>& C) {
     }
 }
 
-vector<int> extractMatrixDimensions(const string& filename) {
+vector<int> find_demensions(const string& filename) {
     ifstream file(filename);
     if (!file.is_open()) {
-        cerr << "Error: Cannot open file " << filename << endl;
+        cerr << "파일열기 실패" << filename << endl;
         exit(EXIT_FAILURE);
     }
 
@@ -79,35 +79,32 @@ vector<int> extractMatrixDimensions(const string& filename) {
                     }
                 }
             }
-            cout << rows << " " << cols << endl;
             if (d.empty()) {
                 d.push_back(rows);
+                d.push_back(cols);
                 temp = cols;
-                cout << rows << "처음" << endl;
             }else if(rows == temp){
-                d.push_back(rows);
+                d.push_back(cols);
                 temp = cols;
-                cout << cols << "그 다음" << endl;
             }
         }
     }
-    for(int i =0; i<d.size(); i++){
-        cout << d[i] << endl;
-    }
+    cout << endl;
     return d;
 }
 
 int main() {
     string filename = "matrix_input.txt";
-    vector<int> dimensions = extractMatrixDimensions(filename);
+    vector<int> dimensions = find_demensions(filename);
 
     if (dimensions.size() < 2) {
-        cerr << "Error: Not enough matrix dimensions found." << endl;
+        cerr << "행렬이 2개이상 있어야 계산 가능합니다." << endl;
         return EXIT_FAILURE;
     }
 
-    int n = dimensions.size() - 1;
-    vector<vector<int>> C(n + 1, vector<int>(n + 1, INT_MAX));
+    int n = dimensions.size();
+
+    vector<vector<int>> C(n, vector<int>(n, INT_MAX));
 
     int minCost = matrixChainOrder(dimensions, C);
 
